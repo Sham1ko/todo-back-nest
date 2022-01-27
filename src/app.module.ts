dd7be8178d660d247b81654a91fcb5c345f6a5a7
Sham1ko <shamshyrak.zholdasbek@gmail.com>
2022-01-27 23:03:11 +0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [ 
-    MongooseModule.forRoot("mongodb://localhost:27017/todos"),
+    MongooseModule.forRoot("mongodb+srv://sham1ko:86femaho@cluster0.69enc.mongodb.net/todos?retryWrites=true&w=majority
+
+    "),
     TodoModule ],
 })
 export class AppModule {}
